ae161e60402ab8079fee52b7034927dc068288b1
Reinaldo Paixao <reinaldo.java@gmail.com>
2025-02-07 21:15:53 -0300

    commit inicial

---
+++ src/main/java/com/realizacontacoes/realizacontacoes_seguro/RealizaContacoesSeguroApplication.java
@@ -0,0 +1,13 @@
+package com.realizacontacoes.realizacontacoes_seguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealizaContacoesSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealizaContacoesSeguroApplication.class, args);
+	}
+
+}
